c0b65f28272779928139a3e644cc85ad22bfa81e
Mu-An Chiou <me@muanchiou.com>
2014-10-06 00:40:22 +0800

    Check once

--- test.js
+++ test.js
@@ -2,37 +2,43 @@ var fs   = require('fs')
 var data = JSON.parse(fs.read('emojis.json'))
 var keys = Object.keys(data)
 
+var testForOrphan = keys.length != 884
+
 if(keys.length != 884) {
-  console.log("Emojis.json has " + keys.length + " entries, but there are 884 emojis.")
+  console.log("There are 884 emojis, but emojis.json has " + keys.length + " entries.")
 }
 
-var testForDups = []
+var dups = []
 
 keys.forEach(function(key) {
-  if(testForDups.indexOf(key) < 0) testForDups.push(key)
+  if(dups.indexOf(key) < 0) dups.push(key)
 })
 
-if(testForDups.length != keys.length) {
-  testForDups.forEach(function(key) {
+var testForDups = dups.length != keys.length
+
+if(testForDups) {
+  dups.forEach(function(key) {
     console.log("There is more than one " + key + " in emojis.json.")
   })
 }
 
-var testForUnnecessities = []
+var unnecessities = []
 
 keys.forEach(function(key) {
   data[key].forEach(function(keyword) {
-    if(key.match(keyword)) testForUnnecessities.push([key, keyword])
+    if(key.match(keyword)) unnecessities.push([key, keyword])
   })
 })
 
-if(testForUnnecessities.length > 0) {
-  testForUnnecessities.forEach(function(arr) {
+var testForUnnecessities = unnecessities.length > 0
+
+if(testForUnnecessities) {
+  unnecessities.forEach(function(arr) {
     console.log("\"" + arr[1] + "\" is unnecessary as it is already part of \"" + arr[0] + "\" and will be matched.")
   })
 }
 
-var buildFailed = keys.length != 884 || testForDups.length != keys.length || testForUnnecessities.length > 0
+var buildFailed = testForOrphan || testForDups || testForUnnecessities
 
 if(buildFailed) {
   phantom.exit(buildFailed)
