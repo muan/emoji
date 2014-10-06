a408b00c11084bc1976aa9937dbf62aa0af9a7eb
Mu-An Chiou <me@muanchiou.com>
2014-10-06 13:10:52 +0800

    They do look like jobs for a test framework but how come it's so hard to find a builtin lightweight framework? Just writing these seems way easier. :(

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
-var fs   = require('fs')
-var data = JSON.parse(fs.read('emojis.json'))
-var keys = fs.read('emojis.json').toString().match(/\"(.+)\"\:/g).map(function(key){return key.replace(/\"|\:/g,'')})
+var fs     = require('fs')
+var data   = JSON.parse(fs.read('emojis.json'))
+var keys   = fs.read('emojis.json').toString().match(/\"(.+)\"\:/g).map(function(key){return key.replace(/\"|\:/g,'')})
+var failed = function() { console.log("\033[91mFAILED\033[0m\n") }
+var passed = function() { console.log("\033[92mPASSED\033[0m\n") }
 
-console.log("\n\n")
+//
+console.log("\nTEST: Correct number of emojis")
 
 var testForOrphan = keys.length != 884
 
 if(keys.length != 884) {
   console.log("There are 884 emojis, but emojis.json has " + keys.length + " entries.")
+  failed()
+} else {
+  passed()
 }
+
+//
+console.log("TEST: No duplicated entries")
 
 var arr = []
 var dups = []
@@ -27,7 +36,13 @@ if(testForDups) {
   dups.forEach(function(key) {
     console.log("There is more than one \"" + key + "\" in emojis.json.")
   })
+  failed()
+} else {
+  passed()
 }
+
+//
+console.log("TEST: No unnecessary keywords")
 
 var unnecessities = []
 
@@ -43,13 +58,17 @@ if(testForUnnecessities) {
   unnecessities.forEach(function(arr) {
     console.log("\"" + arr[1] + "\" is unnecessary as it is already part of \"" + arr[0] + "\" and will be matched.")
   })
+  failed()
+} else {
+  passed()
 }
 
 var buildFailed = testForOrphan || testForDups || testForUnnecessities
 
 if(buildFailed) {
+  console.log(":cry: \033[91mNo good, something failed.\033[0m :boom:\n")
   phantom.exit(buildFailed)
 } else {
-  console.log("\n\n:sparkles::+1:")
+  console.log(":sparkles: \033[96mWho's awesome? You're awesome!\033[0m :+1:\n")
   phantom.exit()
 }
