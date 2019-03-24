b627920b507b19b3957f9cbabea73b4eac15531b
Mu-An Chiou <me@muanchiou.com>
2019-03-24 00:22:59 -0400

    Remove jQuery

--- javascripts/stuff.js
+++ javascripts/stuff.js
@@ -1,34 +1,36 @@
 /* global $, localStorage */
-$(document).on('emoji:ready', function () {
-  var hasFont = hasAppleColorEmoji()
-  $('.input-search').focus()
-  $('.loading').remove()
+var searchField = document.querySelector('.input-search')
+var container = document.querySelector('.emojis-container')
+var url = '//unpkg.com/emojilib@^2.0.0/emojis.json'
+var hasFont = hasAppleColorEmoji()
 
-  if (!hasFont) {
-    prepareTwemoji()
-  }
-
-  $(document).on('click', '.js-emoji', function (evt) {
+document.addEventListener('click', function (evt) {
+  const emoji = evt.target.closest('.js-emoji')
+  if (emoji) {
     var node
     var text
 
     if (evt.shiftKey) {
       getSelection().removeAllRanges()
       var range = document.createRange()
-      node = this.querySelector('.js-emoji-char')
+      node = emoji.querySelector('.js-emoji-char')
       range.selectNodeContents(node)
       getSelection().addRange(range)
 
       text = hasFont ? node.textContent : node.querySelector('img').alt
     } else {
-      node = this.querySelector('.js-emoji-code')
+      node = emoji.querySelector('.js-emoji-code')
       node.select()
       text = node.value
     }
 
     document.execCommand('copy')
     alertCopied(text)
-  })
+  } else if (evt.target.classList.contains('js-hide-text')) {
+    container.classList.toggle('hide-text')
+    var showorhide = container.classList.contains('hide-text') ? 'hide' : 'show'
+    localStorage.setItem('emoji-text-display', !showorhide)
+  }
 })
 
 function prepareTwemoji () {
@@ -38,57 +40,44 @@ function prepareTwemoji () {
     twemoji.parse(document.body)
     document.body.classList.add('twemojified')
   }
-  document.head.appendChild(twemojiScript)
+  document.head.append(twemojiScript)
 }
 
 function alertCopied (emoji) {
-  $('<div class=alert></div>').text('Copied ' + emoji).appendTo('body').fadeIn().delay(1000).fadeOut()
+  var alert = document.createElement('div')
+  alert.classList.add('alert')
+  alert.textContent = `Copied ${emoji}`
+  document.body.append(alert)
+  setTimeout(function() {
+    alert.remove()
+  }, 1000)
 }
 
-function focusOnSearch (e) {
-  var searchField = $('.input-search')[0]
-  if (e.keyCode === 191 && searchField) {
+document.addEventListener('keydown', event => {
+  if (event.key === '/' && searchField) {
     if (searchField.value.length) {
       searchField.selectionStart = 0
       searchField.selectionEnd = searchField.value.length
     }
     searchField.focus()
-    e.preventDefault()
-    return false
+    event.preventDefault()
   }
-}
+})
 
-$.getJSON('/javascripts/emojilib/emojis.json', function (emojis) {
-  var container = $('.emojis-container')
-  Object.keys(emojis).forEach(function (key) {
-    var emoji = emojis[key]
+fetch(url).then(data => data.json()).then(json => {
+  var html
+  for (key in json) {
+    var emoji = json[key]
     if (emoji['category'] === '_custom') return
 
-    var charHTML
-    charHTML = '<div class="js-emoji-char native-emoji" title="' + key + '">' + emoji['char'] + '</div>'
-    container.append('<li class="result emoji-wrapper js-emoji">' +
-      charHTML + '<input type="text" class="js-emoji-code autofocus plain emoji-code" value=":' + key +
-      ':" readonly /><span class="keywords">' + key + ' ' + emoji['keywords'] + '</span></li>')
-  })
-  $(document).trigger('emoji:ready')
-  $('.emojis-container').toggleClass('hide-text', localStorage.getItem('emoji-text-display') === 'false')
-})
-
-$(document).keydown(function (e) { focusOnSearch(e) })
-
-$(document).on('keydown', '.emoji-wrapper input', function (e) {
-  $('.input-search').blur()
-  focusOnSearch(e)
-})
-
-$(document).on('click', '.js-hide-text', function () {
-  $('.emojis-container').toggleClass('hide-text')
-  var showorhide = $('.emojis-container').hasClass('hide-text') ? 'hide' : 'show'
-  localStorage.setItem('emoji-text-display', !$('.emojis-container').hasClass('hide-text'))
-  return false
-})
-
-$(document).on('click', '.js-clear-search, .mojigroup.active', function () {
-  window.location.hash = ''
-  return false
+    html += `<li class="result emoji-wrapper js-emoji">
+      <div class="js-emoji-char native-emoji" title="${key}">${emoji['char']}</div>
+      <input type="text" class="js-emoji-code autofocus plain emoji-code" value=":${key}:" readonly>
+      <span class="keywords">${key} ${emoji['keywords']}</span></li>`
+  }
+  container.innerHTML = html
+  container.classList.toggle('hide-text', localStorage.getItem('emoji-text-display') === 'false')
+  if (!hasFont) prepareTwemoji()
+  document.querySelector('.loading').remove()
+  document.dispatchEvent(new CustomEvent('emoji:ready'))
 })
